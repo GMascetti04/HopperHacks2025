94ff629798ca2758483043380f93f274de020f60
GMascetti04 <giovanni.mascetti54@gmail.com>
2025-02-22 18:42:58 -0500

    added backend server programs (including dummy)

--- server/main.py
+++ server/main.py
@@ -1,16 +1,17 @@
 from ollama import chat
 from ollama import ChatResponse
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import subprocess
-from gtts import gTTS
+
 import os
 
 app = Flask(__name__)
 
 
-def get_motivational_quote():
+def get_motivational_quote(query_text):
     prompt = "Give me a motivational quote."
+    prompt = query_text
 
     response: ChatResponse = chat(
         model="llama3.2",
@@ -24,16 +25,26 @@ def get_motivational_quote():
     return response.message.content
 
 
-@app.route("/generate-motivation", methods=["GET"])
+@app.route("/generate-motivation", methods=["POST"])
 def generate_motivation():
-    motivational_text = get_motivational_quote()
+    
+    request_data = request.get_json()
+    
+    query_text = request_data['query_text']
+    
+    print(query_text)
+    
+    motivational_text = get_motivational_quote(query_text)
 
-    tts = gTTS(motivational_text, lang="en")
-    mp3_filename = "motivational_speech.mp3"
-    tts.save(mp3_filename)
+    #tts = gTTS(motivational_text, lang="en")
+    #mp3_filename = "motivational_speech.mp3"
+    #tts.save(mp3_filename)
 
     return jsonify(
-        {"motivational_text": motivational_text, "audio_url": f"/static/{mp3_filename}"}
+        {"motivational_text": motivational_text, 
+         #
+         # "audio_url": f"/static/{mp3_filename}"
+         }
     )
 
 
