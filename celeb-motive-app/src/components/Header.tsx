7a70d3d730fabf33ef37e41a2dfb4f36495d5e22
mjin08 <maggie.jin22@gmail.com>
2025-02-22 13:01:09 -0500

    Added header component and fixed project structure

---
+++ celeb-motive-app/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ textAlign: "center", padding: "20px", fontSize: "24px", fontWeight: "bold" }}>
+      🎤 Celebrity Motivation 🎬
+    </header>
+  );
+};
+
+export default Header;
